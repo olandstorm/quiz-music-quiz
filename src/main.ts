8ef54e26b11c3a58bdf85d9fbaccbaf8410c3fc2
Ala Hojat Jalali <146936552+alahojat@users.noreply.github.com>
2023-12-22 11:06:27 +0100

    TEST: Change someone's code through review- Update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ interface IExampleArray {
   age: number;
 }
 
-// Detta kommer skapa KONFLIKT
+// Det gör ingenting
 
 // Här skriver vi att vår array med namnet myExampleArray ska följa reglerna (interfacet)
 // i IExampleArray och att det är en array genom att vi sätter [] efter
